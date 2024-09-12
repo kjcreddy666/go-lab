9068f052624e8fc5a8eba4b6ac15db71b33a64f6
jai <kjcreddy666@gmail.com>
2024-09-12 23:06:31 +0530

    week2

--- week2.go
+++ week2.go
@@ -6,22 +6,18 @@ import "fmt"
 
 func printPyramid(n int) {
 	for i := 1; i <= n; i++ {
-		// Print spaces
 		for j := 1; j <= n-i; j++ {
 			fmt.Printf("  ")
 		}
 
-		// Print increasing numbers
 		for j := i; j <= 2*i-1; j++ {
 			fmt.Printf("%d ", j)
 		}
 
-		// Print decreasing numbers
 		for j := 2*i - 2; j >= i; j-- {
 			fmt.Printf("%d ", j)
 		}
 
-		// Move to the next line after each row
 		fmt.Println()
 	}
 }
