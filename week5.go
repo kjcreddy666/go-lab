db14e70471e985c302b8cd96a9bb683e0c9337b1
jai <kjcreddy666@gmail.com>
2024-09-12 22:57:28 +0530

    week5

--- week5.go
+++ week5.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+func printFloydsTriangle(n int) {
+	number := 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d ", number)
+			number++
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
-	
+	var rows int
+	fmt.Print("Enter the number of rows: ")
+	fmt.Scan(&rows)
+	printFloydsTriangle(rows)
 }
