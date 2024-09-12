ccd40f381e261e840739667f524b7888425d3c77
jai <kjcreddy666@gmail.com>
2024-09-12 23:00:00 +0530

    week7

--- week7.go
+++ week7.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+func isPalindrome(num int) bool {
+	original := num
+	reversed := 0
+
+	for num > 0 {
+		digit := num % 10
+		reversed = reversed*10 + digit
+		num /= 10
+	}
+
+	return original == reversed
+}
+
 func main() {
-	
+	var num int
+
+	fmt.Print("Enter an integer: ")
+	fmt.Scan(&num)
+
+	if isPalindrome(num) {
+		fmt.Println(num, "is a palindrome")
+	} else {
+		fmt.Println(num, "is not a palindrome")
+	}
 }
