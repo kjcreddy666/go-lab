15dac0ce358324ad596c2d92f0aca6a026e62bba
jai <kjcreddy666@gmail.com>
2024-09-12 22:58:37 +0530

    week6

--- week6.go
+++ week6.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
+	var str1, str2 string
+
+	fmt.Print("Enter the first string: ")
+	fmt.Scanln(&str1)
 	
+	fmt.Print("Enter the second string: ")
+	fmt.Scanln(&str2)
+
+	result := str1 + str2
+
+	fmt.Println("Concatenated string:", result)
 }
