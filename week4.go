dff743a95e51f9c04080e7bccf97100800e31672
jai <kjcreddy666@gmail.com>
2024-09-12 22:55:55 +0530

    week4

--- week4.go
+++ week4.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func calculateMean(numbers []float64) float64 {
+	var sum float64
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum / float64(len(numbers))
+}
+
+func calculateStandardDeviation(numbers []float64) float64 {
+	mean := calculateMean(numbers)
+	var variance float64
+	for _, num := range numbers {
+		variance += math.Pow(num-mean, 2)
+	}
+	variance /= float64(len(numbers))
+	return math.Sqrt(variance)
+}
+
 func main() {
-	
+	numbers := []float64{10, 12, 23, 23, 16, 23, 21, 16}
+	standardDeviation := calculateStandardDeviation(numbers)
+	fmt.Printf("Standard Deviation: %.2f\n", standardDeviation)
 }
